81a5eef48f3f636a04a5934fc2d2243ec6bdfc15
nelrsf <nsantamaria@tiresia.co>
2022-11-20 09:40:37 -0500

    cors activado

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 import { microservicesEndpoints } from './microservices/microservices.json';
 
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   microservicesEndpoints.forEach(
@@ -11,6 +12,9 @@ async function bootstrap() {
       app.use(microservice.endpointName, createProxyMiddleware(microservice.options));
     }
   );
+  app.enableCors({
+    origin: '*'
+  })
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
